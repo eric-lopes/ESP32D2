cd9a837c734b6fd444e2dd27b11aa36fdb7a2758
Eric Lopes <108433811+eric-lopes@users.noreply.github.com>
2023-10-22 12:51:04 -0300

    Add files via upload

--- ESP32D2.ino
+++ ESP32D2.ino
@@ -14,15 +14,13 @@ VCC  = V5
 GND  = GND */
 
 //Variáveis Globais
-int sdstat=0;        //Status do Cartão de Memória
-int numrede=0;       //Armazena o número de redes e senhas
-String redes[10];    //Armazena SSIDs e Senhas das redes (Até o máximo de 5 redes e senhas)
-int numpasta=0;      //Armazena o Número de Pastas com Música
-String pastas[20];   //Armazena o caminho de cada pasta de música
-int numplaylist=0;   //Armazena o número de músicas em uma pasta
-String playlist[30]; //Armazena uma lista embaralhada de músicas de uma pasta
-
-String teste[50];
+int sdstat=0;               //Status do Cartão de Memória
+int numrede=0;              //Armazena o número de redes e senhas
+String redes[10];           //Armazena SSIDs e Senhas das redes (Até o máximo de 5 redes e senhas)
+int numpasta=0;             //Armazena o Número de Pastas com Música
+String pastas[20];          //Armazena o caminho de cada pasta de música
+int nummusica=0;          //Armazena o número de músicas em uma pasta
+String musicas[30];        //Armazena uma lista embaralhada de músicas de uma pasta
 
 //Funções
 //Inicia o Cartão de Memória
@@ -30,7 +28,7 @@ int sdstart() {
   if(!SD.begin()){
     return -1; //Retorna -1 se houver erro
   }
-  uint64_t cardSize = SD.cardSize() / (1024 * 1024); //Informa o tamanho do cartão.
+  //uint64_t cardSize = SD.cardSize() / (1024 * 1024); //Informa o tamanho do cartão.
   return 1;
 }
 
@@ -55,65 +53,52 @@ void netstart() {
   int net=0;
   if(numrede==0) {
     stat=0;
-    Serial.println("Sem arquivo, sem rede");
   }
   else{
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
     delay(1000);
-    Serial.println("Inicio do Wifi");
     int n = WiFi.scanNetworks();
-    Serial.println("scan done");
     if (n == 0) {
-      Serial.println("no networks found");
       stat=0;
     } 
     else {
-      Serial.print(n);
-      Serial.println(" networks found");
       for (int i = 0; i < n && stat!=1; ++i) {
-        Serial.print("i=");
-        Serial.println(i);
-        Serial.println(WiFi.SSID(i));
-        delay(1000);
-        for(int j=0; j<numrede && stat!=1;j+=2) {
-          Serial.print("j=");
-          Serial.println(j);
-          Serial.println(redes[j]);
-          delay(500);
-          if(WiFi.SSID(i)==redes[j]) {
-            net=j;
+        for(net=0; net<numrede && stat!=1;net+=2) {
+          if(WiFi.SSID(i)==redes[net]) {
             stat=1;
-            Serial.println("ACHEI");
-            Serial.print("stat=");
-            Serial.println(stat);
-            Serial.print("net=");
-            Serial.println(net);
-            Serial.print("SSID=");
-            Serial.println(WiFi.SSID(i));
-            Serial.print("REDE=");
-            Serial.println(redes[net]);
+            break;
           }
-            else {
-            Serial.println("Procurando");
+          else {
             stat=0;
           }
         }
       }
     }
-  } //Termina o scan da rede
-  Serial.println("********************************");
-  Serial.print("stat=");
-  Serial.println(stat);
-  Serial.print("REDE=");
-  Serial.println(redes[net]);
-  Serial.println(redes[net+1]);
-
-  if(stat==0) {
-    Serial.println("Iniciar AP");
   }
-  else {
-    Serial.println("Iniciar Client");
+  if(stat==1) {
+    int aux = redes[net].length()+1;
+    char rede[aux];
+    redes[net].toCharArray(rede,aux);
+    aux = redes[net+1].length()+1;
+    char senha[aux];
+    redes[net+1].toCharArray(senha,aux);
+    WiFi.begin(rede, senha);
+    delay(5000);
+    while (WiFi.status() != WL_CONNECTED) {
+        if(WiFi.status()==6) {
+          stat=0;
+          break;
+        }
+    }
+  }
+  if(stat==0) {
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP("ESP32D2", "R2D2R2D2");
+    delay(200);
+    IPAddress Ip(192, 168, 5, 7);
+    IPAddress NMask(255, 255, 255, 0);
+    WiFi.softAPConfig(Ip, Ip, NMask);
   }
 }
 //Lê as pastas na pasta Music
@@ -137,7 +122,7 @@ int pastaread(fs::FS &fs){
   }
   return a;
 }
-//Lê as músicas dentro de uma pasta
+//Lê as músicas dentro de uma pasta e gera uma lista embaralhada
 int musicaread(fs::FS &fs,int p){
     int a=0;
     File root = fs.open(pastas[p]);
@@ -151,16 +136,16 @@ int musicaread(fs::FS &fs,int p){
     File file = root.openNextFile();
     while(file){
         if(!file.isDirectory()){
-            playlist[a]=file.name();
+            musicas[a]=file.name();
             a++;
         }
         file = root.openNextFile();
     }
   for(int b=0;b<a;b++) {
     int r=random(b,a);
-    String music=playlist[b];
-    playlist[b]=playlist[r];
-    playlist[r]=music;
+    String music=musicas[b];
+    musicas[b]=musicas[r];
+    musicas[r]=music;
   }
   return a;
 }
@@ -171,7 +156,9 @@ int musicaread(fs::FS &fs,int p){
 
 //Toca Música
 void play() {
-    numpasta=pastaread(SD);   
+  int a=0;
+  numpasta=pastaread(SD);
+  nummusica=musicaread(SD,a);   
 }
 
 void websrv () {
@@ -181,36 +168,30 @@ void websrv () {
 
 //Funções Transitórias
 
-void writeFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Writing file: %s\n", path);
-
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("File written");
-    } else {
-        Serial.println("Write failed");
-    }
-    file.close();
-}
-
-void appendFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Appending to file: %s\n", path);
-
-    File file = fs.open(path, FILE_APPEND);
-    if(!file){
-        Serial.println("Failed to open file for appending");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("Message appended");
-    } else {
-        Serial.println("Append failed");
-    }
-    file.close();
+void cria(fs::FS &fs, const char * path){
+  Serial.printf("Writing file: %s\n", path);
+  File file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(redes[0])){
+    Serial.println("File written");
+  } 
+  else {
+    Serial.println("Write failed");
+  }
+  file.close();
+  file = fs.open(path, FILE_APPEND);
+  for(int i=1;i<numrede;i++) {
+    if(file.print(redes[i])){
+      Serial.println("Message appended");
+    } 
+    else {
+      Serial.println("Append failed");
+    }  
+  }
+  file.close();
 }
 
 void renameFile(fs::FS &fs, const char * path1, const char * path2){
@@ -238,7 +219,6 @@ void setup() {
     numrede=netread(SD);
     netstart();
   }
-   
 }
 
 void loop() {
